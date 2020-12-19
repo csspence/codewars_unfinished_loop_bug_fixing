f1aeb2fa20b05dce13507c7a42d71b02fa89ffd9
Crew Spence <csspence123@gmail.com>
2020-12-19 07:13:45 -0600

    Fixed solution.

--- codewars_unfinished_loop_bug_fixing.js
+++ codewars_unfinished_loop_bug_fixing.js
@@ -3,10 +3,10 @@ Unfinished Loop - Bug Fixing #1
 Oh no, Timmy's created an infinite loop! Help Timmy find and fix the bug in his unfinished for loop!
 */
 
-function createArray(number){
+const createArray = (number) => {
   var newArray = [];
   
-  for(var counter = 1; counter <= number;){
+  for(var counter = 1; counter <= number; counter++){
     newArray.push(counter);
   }
   
